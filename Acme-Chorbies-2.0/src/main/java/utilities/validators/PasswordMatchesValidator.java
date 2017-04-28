581ab38415accc098773d4168871044002e52240
alvarogonjim <alvarogonjim95@gmail.com>
2017-04-28 12:43:57 +0200

    Password Matches Validator

--- Acme-Chorbies-2.0/src/main/java/utilities/validators/PasswordMatchesValidator.java
+++ Acme-Chorbies-2.0/src/main/java/utilities/validators/PasswordMatchesValidator.java
@@ -1,20 +1,19 @@
 
-
 package utilities.validators;
+
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
 import form.RegistrationForm;
 import form.RegistrationFormManager;
+
 public class PasswordMatchesValidator implements ConstraintValidator<PasswordMatches, RegistrationForm> {
 
-
-
 	@Override
-	public void initialize(PasswordMatches constraintAnnotation) {
+	public void initialize(final PasswordMatches constraintAnnotation) {
 	}
 	@Override
-	public boolean isValid(RegistrationForm form, ConstraintValidatorContext context) {
+	public boolean isValid(final RegistrationForm form, final ConstraintValidatorContext context) {
 		return form.getPassword().equals(form.getPasswordCheck());
 	}
 }
